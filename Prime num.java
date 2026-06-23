0e02047be0aad2224a70a75681c8e49a9b158ff7
Aswinireddy4571 <122337756+Aswinireddy4571@users.noreply.github.com>
2026-06-23 09:40:40 +0530

    Prime num.java

---
+++ Prime num.java
@@ -0,0 +1,20 @@
+package oldjunit;
+
+import org.junit.Test;
+import static org.junit.Assert.*; public class PrimeNum {
+public boolean isPrime(int n) { if(n <= 1)
+return false;
+for (int i = 2; i <= Math.sqrt(n); i++) { if(n % i == 0)
+return false;
+}
+return true;
+}
+@Test
+public void testPrimeTrue() { assertTrue(isPrime(13));
+}
+@Test
+public void testPrimeFalse() { assertFalse(isPrime(12));
+}
+@Test
+public void testPrimeOne() { assertFalse(isPrime(1));
+}}
